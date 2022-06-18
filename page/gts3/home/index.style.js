36a251f3d73261b08b712268e04a65a5aa910bcc
akryvtsun <kontiky@gmail.com>
2022-06-18 18:15:30 +0300

    minor changes

--- page/gts3/home/index.style.js
+++ page/gts3/home/index.style.js
@@ -1,12 +1,10 @@
-import { gettext } from "i18n"
-
-export const { width: DEVICE_WIDTH, height: DEVICE_HEIGHT } = hmSetting.getDeviceInfo()
+export const { width, height, deviceName } = hmSetting.getDeviceInfo()
 
 export const TEXT_STYLE = {
-  text: gettext('appName'),
+  text: deviceName,
   x: 42,
   y: 200,
-  w: DEVICE_WIDTH - 42 * 2,
+  w: width - 42 * 2,
   h: 100,
   color: 0xffffff,
   text_size: 36,
