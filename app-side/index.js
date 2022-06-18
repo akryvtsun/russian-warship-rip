d8a0991d868e9e1284705988007d73cce99c3644
akryvtsun <kontiky@gmail.com>
2022-06-18 19:30:45 +0300

    initial fetch

--- app-side/index.js
+++ app-side/index.js
@@ -1,11 +1,18 @@
 AppSideService({
-  onInit() {
-    console.log('app side service invoke onInit')
-  },
-  onRun() {
-    console.log('app side service invoke onRun')
-  },
-  onDestroy() {
-    console.log('app side service invoke onDestroy')
-  }
+    onInit() {
+        console.log('app side service invoke onInit')
+        fetch('https://russianwarship.rip/api/v1/statistics/latest')
+            .then(function (data) {
+                // var sbb = data.body.split(/"forwardConversionRate":|,"backwardConversionRate"/)
+                // sb = sbb[1]
+            })
+    },
+
+    onRun() {
+        console.log('app side service invoke onRun')
+    },
+
+    onDestroy() {
+        console.log('app side service invoke onDestroy')
+    }
 })
